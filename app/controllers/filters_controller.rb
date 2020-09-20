ea12bef198f0dc30eba235229695d500bbb4f79f
Alexander Encinas <alexcromwell777@gmail.com>
2020-09-20 12:55:55 -0400

    filter page has products and category selection

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,6 +2,11 @@ class FiltersController < ApplicationController
   def show
     # Parameters: {"id"=>"421"}
     @filter = Filter.find(params[:id])
-    
+    @filters = Filter.all
+    @products =  Filter.find(params[:id]).products
+    @categories = @products.map do |prod|
+      prod.category
+    end
+    @uniq = @categories.uniq
   end
 end
