7601daef92ed8137c8610045f43ddc36c716bc6c
Alexander Encinas <alexcromwell777@gmail.com>
2020-09-21 14:27:15 -0400

    branch completed

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,27 +1,30 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-libresante');
-  const brand = document.querySelector('.brand');
+  const whiteBrand = document.querySelector('.white-brand');
+  const blackBrand = document.querySelector('.black-brand');
   const icons = document.querySelectorAll('.icon-white');
   const links = document.querySelectorAll('.nav-link');
-
+  const banner = document.querySelector('.banner-container');
 
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= banner.offsetHeight) {
         navbar.classList.add('navbar-libresante-white');
-        brand.classList.add('brand-black');
+        
+        whiteBrand.classList.add('brand-hide');
+        blackBrand.classList.remove('brand-hide');
+        
         icons.forEach(e => e.classList.add('icon-change'));
-        // icon.classList.add('icon-change');
         links.forEach(e => e.classList.add('link-change'));
         
-        // link.classList.add('link-change');
       } else {
         navbar.classList.remove('navbar-libresante-white');
-        brand.classList.remove('brand-black');
+        
+        whiteBrand.classList.remove('brand-hide');
+        blackBrand.classList.add('brand-hide');
+
         icons.forEach(e => e.classList.remove('icon-change'));
-        // icon.classList.remove('icon-change');
         links.forEach(e => e.classList.remove('link-change'));
-        // link.classList.remove('link-change');
       }
     });
   }
